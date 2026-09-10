46ca9e8a2802be6d8b4c10e4f42529c5bf88558b
Pranav-Hirey <pranavhirey13@gmail.com>
2026-09-10 21:45:33 +0530

    Add program to check for Strong numbers

---
+++ Day22/Day22code1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, original, digit, i, factorial, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    original = num;
+
+    while(num != 0)
+    {
+        digit = num % 10;
+
+        factorial = 1;
+
+        for(i = 1; i <= digit; i++)
+        {
+            factorial = factorial * i;
+        }
+
+        sum = sum + factorial;
+
+        num = num / 10;
+    }
+
+    if(sum == original)
+    {
+        printf("%d is a Strong number", original);
+    }
+    else
+    {
+        printf("%d is not a Strong number", original);
+    }
+
+    return 0;
+}
