0322f91d3fb9d2f608c2e65fb3d1a18893ead7cb
Pranav-Hirey <pranavhirey13@gmail.com>
2026-09-16 19:47:01 +0530

    Add diamond pattern printing in Day26code2.c
    This program prints a diamond shape made of asterisks.

---
+++ Day26/Day26code2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, stars;
+
+    for(i = 1; i <= 5; i++)
+    {
+        stars = 2 * i - 1;
+
+        for(j = 1; j <= stars; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    for(i = 4; i >= 1; i--)
+    {
+        stars = 2 * i - 1;
+
+        for(j = 1; j <= stars; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
