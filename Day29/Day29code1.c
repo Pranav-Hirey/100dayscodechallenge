f381dcceb2811bad0101400b919f45f829b86275
Pranav-Hirey <pranavhirey13@gmail.com>
2026-09-19 22:56:43 +0530

    Implement array max and min finder in C
    This program reads an array of integers and calculates the maximum and minimum values.

---
+++ Day29/Day29code1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], n, i;
+    int maximum, minimum;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements:\n");
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    maximum = arr[0];
+    minimum = arr[0];
+
+    for(i = 1; i < n; i++)
+    {
+        if(arr[i] > maximum)
+        {
+            maximum = arr[i];
+        }
+
+        if(arr[i] < minimum)
+        {
+            minimum = arr[i];
+        }
+    }
+
+    printf("Maximum element = %d\n", maximum);
+    printf("Minimum element = %d", minimum);
+
+    return 0;
+}
