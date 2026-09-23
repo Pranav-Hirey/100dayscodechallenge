e09218fb28bd28b65eb2674a142d2fcd04307aa9
Pranav-Hirey <pranavhirey13@gmail.com>
2026-09-23 22:05:15 +0530

    Add digit frequency counter in Day32ccode1.c
    This program counts the occurrences of each digit in an integer and identifies the digit that occurs the most.

---
+++ Day32/Day32ccode1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, digit;
+    int count[10] = {0};
+    int i, max = 0, maxDigit = 0;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    while(num != 0)
+    {
+        digit = num % 10;
+        count[digit]++;
+        num = num / 10;
+    }
+
+    for(i = 0; i < 10; i++)
+    {
+        if(count[i] > max)
+        {
+            max = count[i];
+            maxDigit = i;
+        }
+    }
+
+    printf("Digit occurring most times = %d", maxDigit);
+
+    return 0;
+}
