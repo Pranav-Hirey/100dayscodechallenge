e4e6f0d6d9e35a0c374d887c4b76406cb6389508
Pranav-Hirey <pranavhirey13@gmail.com>
2026-09-25 22:38:53 +0530

    Add insertion function for sorted array

---
+++ Day33/Day33code1.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], n, element;
+    int i, position;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements in sorted order: ");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter element to insert: ");
+    scanf("%d", &element);
+
+    position = n;
+
+    for(i = 0; i < n; i++)
+    {
+        if(element < arr[i])
+        {
+            position = i;
+            break;
+        }
+    }
+
+    for(i = n; i > position; i--)
+    {
+        arr[i] = arr[i - 1];
+    }
+
+    arr[position] = element;
+    n++;
+
+    printf("Array after insertion: ");
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
